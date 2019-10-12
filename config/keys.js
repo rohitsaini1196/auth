c2813559ad6050e8c9fff491da225e2c535be602
Rohit Saini <31476481+rohitsaini1196@users.noreply.github.com>
2019-10-12 05:53:29 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 dbPassword =
-  "YOUR_MONGODB_COLLECTION";
+  "mongodb+srv://fortwin:qwer5678@cluster0-0d0yw.mongodb.net/test?retryWrites=true&w=majority";
 
 module.exports = {
   mongoURI: dbPassword
